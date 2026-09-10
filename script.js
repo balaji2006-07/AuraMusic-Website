102e49cbd513546df94a247d2d0b55a530233d88
balaji2006-07 <pulsarbalaji2006@gmail.com>
2026-09-10 23:17:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+// Aura Music website
+// Basic smooth interaction
+
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+    link.addEventListener("click", function (e) {
+
+        const target = document.querySelector(this.getAttribute("href"));
+
+        if (target) {
+            e.preventDefault();
+
+            target.scrollIntoView({
+                behavior: "smooth"
+            });
+        }
+    });
+});
